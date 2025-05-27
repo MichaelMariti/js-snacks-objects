8fc62b873d84b8dbba0419f8639330052817757a
MichaelMariti <michael.mariti1@gmail.com>
2025-05-27 19:49:49 +0200

    error

--- snack-02.js
+++ snack-02.js
@@ -2,14 +2,15 @@ console.log('snack-02')
 
 const parole = ['pippo', 'PLUTO', 'Paperino'] // Pippo, Pluto, Paperino
 const paroleFormattate = []
+console.log = parole
 
 for (let i = 0; i = parole.length; i++) {
     const parola = parole[i]
     console.log(i, parola)
 
     //prendere la prima lettera della parola e metterla maiuscola
-    const firstChar = parola.charAt(0)
-    firstChar = firstChar.toUpperCase()
+    const firstChar = parola.charAt(0).toUpperCase()
+    // firstChar = firstChar.toUpperCase()
     console.log(firstChar)
     //prendere dalla seconda lettera in poi e mettere tutto minuscolo
     const restoParola = parola.slice(1)
